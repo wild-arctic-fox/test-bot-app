de7ce4f2f5e2f5ff40f63a35b5f007083063013d
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2020-11-07 22:57:07 +0200

    init commit

---
+++ handler.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const rp = require('request-promise');
+
+
+const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+
+
+async function sendToUser(chat_id, text) {
+  const options = {
+    method: 'GET',
+    uri: `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+    qs: {
+      chat_id,
+      text
+    }
+  };
+
+  return rp(options);
+}
+
+
+module.exports.shortbot = async event => {
+  const body = JSON.parse(event.body);
+  const { chat, text } = body.message;
+  console.log(body);
+  if (text) {
+    let message = '';
+    try {
+      message = `Hello, ${text}!`;
+    } catch (error) {
+      message = `Input: ${text}, \nError: ${error.message}`;
+    }
+
+    await sendToUser(chat.id, message);
+  } else {
+    await sendToUser(chat.id, 'Text message is expected.');
+  }
+
+  return { statusCode: 200 };
+};
